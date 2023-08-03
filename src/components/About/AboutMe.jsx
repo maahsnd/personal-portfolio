a922b8c3d016df6dd686b75647ef301473653961
mrwindupbirdesq <nmaahs11@gmail.com>
2023-08-03 16:45:43 -0700

    Convert button to link

--- src/components/About/AboutMe.jsx
+++ src/components/About/AboutMe.jsx
@@ -20,8 +20,14 @@ function AboutMe() {
             me about me about me about me about me about me about me about me
           </p>
 
-          <img src={githubLink} className={styles.icon} />
-          <img src={linkedinLink} className={styles.icon} />
+          <a href="https://github.com/maahsnd">
+            <img src={githubLink} className={styles.icon} />
+          </a>
+
+          <a href="https://www.linkedin.com/in/nicholas-maahs/">
+            {' '}
+            <img src={linkedinLink} className={styles.icon} />
+          </a>
         </div>
       </div>
     </>
