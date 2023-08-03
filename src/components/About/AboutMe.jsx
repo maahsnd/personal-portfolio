21bacabacd0668e19569de4b1a8e385c10eaeb4d
mrwindupbirdesq <nmaahs11@gmail.com>
2023-08-03 15:36:58 -0700

    Add bio section

--- src/components/About/AboutMe.jsx
+++ src/components/About/AboutMe.jsx
@@ -3,9 +3,18 @@ import styles from './AboutMe.module.css';
 function AboutMe() {
   return (
     <>
-      <div className={styles.frame}>
-        <img src="/prof_photo.jpeg" className="logo" alt="Photo of author" />
-        <span className={styles.caption}>Nicholas Maahs</span>
+      <div className={styles.container}>
+        <div className={styles.frame}>
+          <img src="/prof_photo.jpeg" className="logo" alt="Photo of author" />
+          <span className={styles.caption}>Nicholas Maahs</span>
+        </div>
+        <div className={styles.bio}>
+          <h3>About me</h3>
+          <p>
+            about me about me about me about me about me about me about me about
+            me about me about me about me about me about me about me about me
+          </p>
+        </div>
       </div>
     </>
   );
