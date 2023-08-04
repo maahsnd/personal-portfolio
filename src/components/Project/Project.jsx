695ef5ab5cb4ddd2eb84a086e28d4fa6a599b8f7
mrwindupbirdesq <nmaahs11@gmail.com>
2023-08-04 13:04:11 -0700

    Scaffold project card

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -1,10 +1,18 @@
 import styles from 'Project.module.css';
 
-const Project = () => {
+const Project = (props) => {
+  /*     ({ repoLink, siteLink, projectName, description } = props);
+    ({screenShotSrc} = props) */
 
-
-
-    return (
-
-    )
-}
+  return (
+    <div className={styles.container}>
+      <div className={styles.screenshot}></div>
+      <div className={styles.projectDetails}>
+        <div className={topBar}>
+          <h3 className={styles.projectName}>{projectName}</h3>
+          <div className={buttons}>{/* buttons */}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
