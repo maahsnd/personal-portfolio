ddb95ca5370ab01f79f650d95347f7e6d08bb043
mrwindupbirdesq <nmaahs11@gmail.com>
2023-08-07 13:49:01 -0700

    Pass project info as props

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,11 +1,15 @@
 import styles from './Portfolio.module.css';
 import Project from '../Project/Project';
+import ProjectData from './ProjectData';
 
 const Portfolio = () => {
+  const data = ProjectData();
+  console.log(data);
+
   return (
     <div className={styles.container}>
-      <Project />
-      <Project />
+      <Project projectInfo={data[0]} />
+      <Project projectInfo={data[1]} />
       <Project />
       <Project />
     </div>
