067c5c616e6c715b868c103fa4d3498bcb88b1ed
mrwindupbirdesq <nmaahs11@gmail.com>
2023-08-07 13:37:35 -0700

    Create file

---
+++ src/components/Portfolio/ProjectData.jsx
@@ -0,0 +1,26 @@
+const ProjectData = () => {
+  const data = [
+    {
+      title: 'Avatar Memory Game',
+      description:
+        'On each click, the cards shuffle. Click a card more than once and your score resets.',
+      siteLink: 'https://maahsnd.github.io/memory-card/',
+      repoLink: 'https://github.com/maahsnd/memory-card/tree/Main',
+      screenShotSrc:
+        'https://res.cloudinary.com/dscsiijis/image/upload/c_scale,h_200,q_100/v1691439766/Screen_Shot_2023-08-07_at_10.01.48_AM_d6tpp2.jpg'
+    },
+    {
+      title: 'Shopping Cart App',
+      description:
+        'A basic e-commerce build, featuring a homepage and product page where the users can add items to a cart. Note: content null for time being; site does not implement checkout.',
+      siteLink: 'https://maahsnd.github.io/shopping-cart/',
+      repoLink: 'https://github.com/maahsnd/shopping-cart',
+      screenShotSrc:
+        'https://res.cloudinary.com/dscsiijis/image/upload/c_scale,q_100,w_280/v1691439759/Screen_Shot_2023-08-07_at_10.02.01_AM_ggrsmf.jpg'
+    }
+  ];
+
+  return data;
+};
+
+export default ProjectData;
